5d93ef5287a5fbc1ddc0c19320d6376b4cf85b03
Gustavo Pasqualetti <49802004863@info.local>
2023-03-30 10:53:43 -0300

    projeto senha

---
+++ senha-projeto/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine($"Digite sua senha:");
+float senha= float.Parse(Console.ReadLine());
+
+if (senha == 1234)
+{
+Console.WriteLine($"ACESSO PERMITIDO");
+
+}
+
+else
+{
+Console.WriteLine($"ACESSO NEGADO");
+
+}
